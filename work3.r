ad4995a6ab72c80e188f8e7781f8a667d14f54fc
Obr00007576 <51303014+Obr00007576@users.noreply.github.com>
2023-04-20 21:10:57 +0800

    0.0.6

---
+++ work3.r
@@ -0,0 +1,15 @@
+library(kknn)
+set.seed(333)
+
+train_data <- read.table("svmdata4.txt", colClasses=c("NULL", rep("double", 2), "factor"), skip = 1)
+colnames(train_data) <- c("V1", "V2", "Color")
+
+test_data <- read.table("svmdata4test.txt", colClasses=c("NULL", rep("double", 2), "factor"), skip = 1)
+colnames(test_data) <- c("V1", "V2", "Color")
+
+
+
+model <- train.kknn(Color ~ ., data = train_data, kmax = 30)
+pred <- as.data.frame(predict(model, test_data[, -3]))
+plot(test_data$V1, test_data$V2, pch=21, bg=c("red","green") [unclass(pred[, c(1)])], main="My train data")
+error_rate <- sum(pred[, c(1)] != test_data[, c(3)]) / nrow(test_data)
